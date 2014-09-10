6ae07ad5688dbcb6b8cc073edd20849b2213395f
Chris Mattmann <mattmann@apache.org>
2014-09-09 21:36:36 -0700

    refactor code to be based on existing verify scripts - removed functionality for auto import of KEYS - we can do better by downloading from the https://people.apache.org/keys/group/. we should also tie this into stage_apache_rc - if we map this in there, we can do the whole thing end to end.

--- verify_rc.sh
+++ verify_rc.sh
@@ -3,49 +3,18 @@
 # Usage:
 #   ./verify-rc.sh RELEASE_DIRECTORY
 #
-# It is expected that you have md5deep installed on your system.
-# MD5 on OS X 10.9.2 didn't have the ability to verify checksums.
-# You can visit the md5deep website for installation instructions
-# or use Homebrew if you're on OS X.
-#
-# http://md5deep.sourceforge.net/
-#
-# When attempting to import keys for signature verification it is
-# assumed that your release is (at the minimum) distributed as a .zip.
-# If this is not the case, your key file will not be imported. Adjust
-# the extraction code as necessary to accommodate your project's preferred
-# distribution format(s).
 
 echo 
 echo "------------------"
 echo "  Verifying MD5s  "
 echo "------------------"
-for f in $1/*.md5; do
-    echo
-    md5deep -M $f ${f%.md5}
 
-    if [ $? -eq 0 ]; then
-        echo "MD5 verified"
-    else
-        echo "MD5 failed to verify"
-    fi
-done
+./verify_md5_checksums
 
 echo 
 echo "------------------------"
 echo "  Verifying Signatures  "
 echo "------------------------"
-unzip $1/*.zip -d ./test >/dev/null 2>&1
 
-if [ -e ./test/KEYS ]; then
-    echo "Importing KEYS file from release artifact(s)"
-    gpg --import ./test/KEYS
-    echo ""
-fi
+./verify_gpg_sigs
 
-for f in $1/*.asc; do
-    echo ""
-    gpg --verify $f
-    echo ""
-done
-rm -rf ./test
